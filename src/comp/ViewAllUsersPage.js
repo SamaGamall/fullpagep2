7015d9d236a99e830c8302c93a44bedd4331a7ac
SamaGamall <samagamal024@gmail.com>
2023-06-06 17:10:18 +0300

    essage

--- src/comp/ViewAllUsersPage.js
+++ src/comp/ViewAllUsersPage.js
@@ -1,49 +1,78 @@
 import React from 'react';
-import { Grid, Card, CardHeader, CardContent, CardActions, Typography, Button } from '@mui/material';
+import { ChakraProvider, Grid, Box, Flex, Avatar, Heading, Text, IconButton, Image, Button } from '@chakra-ui/react';
+import { BsThreeDotsVertical } from 'react-icons/bs';
+import { BiLike, BiChat, BiShare } from 'react-icons/bi';
 
 const ViewAllUsersPage = () => {
   return (
-    <Grid container spacing={4}>
-      <Grid item xs={12} sm={6} md={4}>
-        <Card>
-          <CardHeader>
-            <Typography variant="h6">Customer dashboard</Typography>
-          </CardHeader>
-          <CardContent>
-            <Typography variant="body1">View a summary of all your customers over the last month.</Typography>
-          </CardContent>
-          <CardActions>
-            <Button variant="contained" color="primary">View here</Button>
-          </CardActions>
-        </Card>
+    <ChakraProvider>
+      <Grid templateColumns='repeat(auto-fill, minmax(200px, 1fr))' gap={4}>
+        <Box
+          maxW='md'
+          borderWidth='1px'
+          borderRadius='lg'
+          overflow='hidden'
+          boxShadow='md'
+          _hover={{ boxShadow: 'lg' }}
+        >
+          <Flex p={4} alignItems='center' justifyContent='space-between'>
+            <Flex alignItems='center' flexWrap='wrap'>
+              <Avatar name='Segun Adebayo' src='https://bit.ly/sage-adebayo' />
+              <Box ml={4}>
+                <Heading size='sm'>Segun Adebayo</Heading>
+                <Text>Creator, Chakra UI</Text>
+              </Box>
+            </Flex>
+            <IconButton
+              variant='ghost'
+              colorScheme='gray'
+              aria-label='See menu'
+              icon={<BsThreeDotsVertical />}
+            />
+          </Flex>
+          <Box p={4}>
+            <Text>
+              With Chakra UI, I wanted to sync the speed of development with the speed
+              of design. I wanted the developer to be just as excited as the designer to
+              create a screen.
+            </Text>
+          </Box>
+          <Image
+            w='full'
+            h={64}
+            objectFit='cover'
+            src='https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+            alt='Chakra UI'
+          />
+          <Flex p={4} justifyContent='space-between'>
+            <Button
+              flex='1'
+              variant='ghost'
+              leftIcon={<BiLike />}
+              _hover={{ backgroundColor: 'blue.500', color: 'white' }}
+            >
+              Like
+            </Button>
+            <Button
+              flex='1'
+              variant='ghost'
+              leftIcon={<BiChat />}
+              _hover={{ backgroundColor: 'green.500', color: 'white' }}
+            >
+              Comment
+            </Button>
+            <Button
+              flex='1'
+              variant='ghost'
+              leftIcon={<BiShare />}
+              _hover={{ backgroundColor: 'purple.500', color: 'white' }}
+            >
+              Share
+            </Button>
+          </Flex>
+        </Box>
       </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <Card>
-          <CardHeader>
-            <Typography variant="h6">Customer dashboard</Typography>
-          </CardHeader>
-          <CardContent>
-            <Typography variant="body1">View a summary of all your customers over the last month.</Typography>
-          </CardContent>
-          <CardActions>
-            <Button variant="contained" color="primary">View here</Button>
-          </CardActions>
-        </Card>
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <Card>
-          <CardHeader>
-            <Typography variant="h6">Customer dashboard</Typography>
-          </CardHeader>
-          <CardContent>
-            <Typography variant="body1">View a summary of all your customers over the last month.</Typography>
-          </CardContent>
-          <CardActions>
-            <Button variant="contained" color="primary">View here</Button>
-          </CardActions>
-        </Card>
-      </Grid>
-    </Grid>
+    </ChakraProvider>
   );
 };
 
--- src/comp/header.js
+++ src/comp/header.js
@@ -1,59 +1,121 @@
-import React, { useState } from 'react';
-import { Link, NavLink  } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './Header.css';
-import {FaBars} from 'react-icons/fa';
-import AnchorLink from "react-anchor-link-smooth-scroll";
+import { FaBars } from 'react-icons/fa';
 import MenuIcon from '@mui/icons-material/Menu';
-
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Header = () => {
-  
+  const [activeLink, setActiveLink] = useState('');
+  const location = useLocation();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const sections = document.querySelectorAll('section');
+      
+      sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+
+        if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+          setActiveLink(section.id);
+        }
+      });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    setActiveLink('');
+  }, [location.pathname]);
+
   function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "nav-bar") {
-      x.className += " responsive";
+    var x = document.getElementById('myTopnav');
+    if (x.className === 'nav-bar') {
+      x.className += ' responsive';
     } else {
-      x.className = "nav-bar";
+      x.className = 'nav-bar';
     }
   }
+
   return (
     <>
-  
-  <header>
-    <nav className="nav-bar" id="myTopnav">
-      <Link className="Mainlink" to="/" style={{ color: "black" }}>
-        home
-      </Link>
-      <Link className="Mainlink" to="/donationlocation " style={{ color: "black" }}>
-        Donor Center Location
-      </Link>
-      <AnchorLink className="Mainlink" href="#about" style={{ color: "black" }}>
-        About Us
-      </AnchorLink>
-      <AnchorLink className="Mainlink" href="#blood" style={{ color: "black" } } >
-        Instructions
-      </AnchorLink>
-      
-      <Link
-        className=" btn1-1"
-        to="/Pagelogin"
-      >
-        donate
-      </Link>
-      
-
-      <a href="javascript:void(0);" className="icon2" onClick={myFunction}>
-      <MenuIcon  />
-      </a>
-
-      
-    </nav>
-  </header>
-  
-</>
-
+      <header>
+        <nav className="nav-bar" id="myTopnav">
+          {location.pathname === '/' ? (
+            <Link
+              className={`Mainlink ${activeLink === 'home' ? 'active' : ''}`}
+              href="#"
+              style={{ color: 'black' }}
+            >
+              home
+            </Link>
+          ) : (
+            <Link
+              className="Mainlink"
+              to="/"
+              style={{ color: 'black' }}
+            >
+              home
+            </Link>
+          )}
+          <Link
+            className={`Mainlink ${activeLink === 'donor-location' ? 'active' : ''}`}
+            to="/donationlocation"
+            style={{ color: 'black' }}
+          >
+            Donor Center Location
+          </Link>
+          {location.pathname === '/' ? (
+            <AnchorLink
+              className={`Mainlink ${activeLink === 'about' ? 'active' : ''}`}
+              href="#about"
+              style={{ color: 'black' }}
+            >
+              About Us
+            </AnchorLink>
+          ) : (
+            <Link
+              className="Mainlink"
+              to="/"
+              style={{ color: 'black' }}
+            >
+              About Us
+            </Link>
+          )}
+          {location.pathname === '/' ? (
+            <AnchorLink
+              className={`Mainlink ${activeLink === 'instructions' ? 'active' : ''}`}
+              href="#blood"
+              style={{ color: 'black' }}
+            >
+              Instructions
+            </AnchorLink>
+          ) : (
+            <Link
+              className="Mainlink"
+              to="/"
+              style={{ color: 'black' }}
+            >
+              Instructions
+            </Link>
+          )}
+          <Link className="btn1-1" to="/Pagelogin">
+            donate
+          </Link>
+          <a href="javascript:void(0);" className="icon2" onClick={myFunction}>
+            <MenuIcon />
+          </a>
+        </nav>
+      </header>
+    </>
   );
-}
+};
 
 export default Header;
-
